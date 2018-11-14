06389e0d55c38b843c0ff30eeaef3cf918415511
Maggie Rose <Gosik@Mags-Computer.local>
2018-11-14 17:07:35 +0100

    can add, delete, mark with line through & opacity

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import React from 'react'
 export default class App extends React.Component {
   state = {
     toDos: [],
-    toDo: '',
+    toDo: ' ',
   }
     handleSubmit = event =>{
       event.preventDefault()
       let {toDos, toDo} = this.state;
-      toDos.push(toDo);
+      toDos.push({text: toDo, line: 'none', opacity: 1});
       this.setState({toDos:toDos, toDo:''});
     }
 
@@ -22,23 +22,35 @@ export default class App extends React.Component {
       this.setState({toDos:toDos});
     }
 
-    handleClick = index => {
+    handleLine = index => {
+      let arr = this.state.toDos
+      if (arr[index].line === 'none'){
+        arr[index].line = 'line-through'
+        arr[index].opacity = .3
+        }else{
+          arr[index].line = 'none'
+          arr[index].opacity = 1
 
-      
+      }
+      this.setState({toDos:arr})
     }
 
-
-  
-
   render(){
-   
+  
     return (
-      <div>
+      <div className='mylist'>
         <h1 className='title'>Maggie's Todos!!!</h1>
 
           <div className='input'>
             <ol>
-              {this.state.toDos.map((toDo, i)=> <li  key={i}>{toDo}</li>)}
+              {this.state.toDos.map((toDo, i)=> <li 
+                style={{textDecoration: toDo.line, opacity: toDo.opacity}}
+                key={i}>
+                <span onClick={()=>this.handleLine(i)}>{toDo.text}</span>
+                <span onClick={()=>this.handleDelete(i)}>
+                <i className="fas fa-trash-alt"></i>
+                </span></li>
+              )}
             </ol>
             
               <form onSubmit = {this.handleSubmit}>
@@ -48,13 +60,6 @@ export default class App extends React.Component {
               <button>Add Todo!</button>
               </form>
           </div>
-
-          
-          <div className='deleteTodo'>
-
-          </div>
-
-
 
 
       </div>
